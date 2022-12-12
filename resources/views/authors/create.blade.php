8d4a517e45fdf99b63a4a08a8bc3959d4248ea3c
Fedora Yoshe Juandy <fedorajuandy@gmail.com>
2022-12-13 00:35:56 +0700

    finish author; modify view

--- resources/views/authors/create.blade.php
+++ resources/views/authors/create.blade.php
@@ -7,28 +7,28 @@
 <form action="{{ route('authors.store') }}" method="POST">
     @csrf
     <div class="row">
-        <div class="col-md-6 mb-3">
+        <div class="col-md-12 mb-3">
             <label for="title">Id</label>
             <input type="text" class="form-control @error('id') is-invalid @enderror" name="id" id="id" value="{{ old('id') }}">
             @error('id')
                 <div class="text-danger">{{ $message }}</div>
             @enderror
         </div>
-        <div class="col-md-6 mb-3">
+        <div class="col-md-12 mb-3">
             <label for="title">Nama</label>
             <input type="text" class="form-control @error('nama') is-invalid @enderror" name="nama" id="nama" value="{{ old('nama') }}">
             @error('nama')
                 <div class="text-danger">{{ $message }}</div>
             @enderror
         </div>
-        <div class="col-md-6 mb-3">
+        <div class="col-md-12 mb-3">
             <label for="title">Kota</label>
             <input type="text" class="form-control @error('kota') is-invalid @enderror" name="kota" id="kota" value="{{ old('kota') }}">
             @error('kota')
                 <div class="text-danger">{{ $message }}</div>
             @enderror
         </div>
-        <div class="col-md-6 mb-3">
+        <div class="col-md-12 mb-3">
             <label for="title">Negara</label>
             <input type="text" class="form-control @error('negara') is-invalid @enderror" name="negara" id="negara" value="{{ old('negara') }}">
             @error('negara')
--- resources/views/authors/edit.blade.php
+++ resources/views/authors/edit.blade.php
@@ -8,23 +8,30 @@
         @csrf
         @method('PUT')
         <div class="row">
-            <div class="col-md-6 mb-3">
+            <div class="col-md-12 mb-3">
+                <label for="title">Id</label>
+                <input type="text" class="form-control @error('id') is-invalid @enderror" name="id" id="id" value="{{ old('id') ?? $author->id }}">
+                @error('id')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="col-md-12 mb-3">
                 <label for="title">Nama</label>
                 <input type="text" class="form-control @error('nama') is-invalid @enderror" name="nama" id="nama" value="{{ old('nama') ?? $author->nama }}">
                 @error('nama')
                     <div class="text-danger">{{ $message }}</div>
                 @enderror
             </div>
-            <div class="col-md-6 mb-3">
+            <div class="col-md-12 mb-3">
                 <label for="title">Kota</label>
-                <input type="text" class="form-control @error('kota') is-invalid @enderror" name="kota" id="kota" value="{{ old('kota') }}">
+                <input type="text" class="form-control @error('kota') is-invalid @enderror" name="kota" id="kota" value="{{ old('kota') ?? $author->kota }}">
                 @error('kota')
                     <div class="text-danger">{{ $message }}</div>
                 @enderror
             </div>
-            <div class="col-md-6 mb-3">
+            <div class="col-md-12 mb-3">
                 <label for="title">Negara</label>
-                <input type="text" class="form-control @error('negara') is-invalid @enderror" name="negara" id="negara" value="{{ old('negara') }}">
+                <input type="text" class="form-control @error('negara') is-invalid @enderror" name="negara" id="negara" value="{{ old('negara') ?? $author->negara }}">
                 @error('negara')
                     <div class="text-danger">{{ $message }}</div>
                 @enderror
