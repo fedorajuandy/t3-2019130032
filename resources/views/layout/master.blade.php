af47c7b3165a0f1119dfce417ca65c6ff1238a7a
Fedora Yoshe Juandy <fedorajuandy@gmail.com>
2022-12-13 02:31:55 +0700

    modify css cache

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -6,7 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>@yield('title', 'XYZLibrary') | XYZ Library</title>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/style-modified.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/style-modified.css?v=').time() }}">
         @stack('css_after')
     </head>
 
